550624cfe7be50f74abd20dbe899c7e12a20bfbb
Nefta11 <neftaliarturohernandez@gmail.com>
2025-03-21 09:57:56 -0600

    feat: agregar carga de datos con esqueleto y manejo de estado vacío en FavoritesScreen

--- app/screens/dashbordScreens/FavoritesScreen.jsx
+++ app/screens/dashbordScreens/FavoritesScreen.jsx
@@ -1,20 +1,48 @@
-import React, { useEffect } from 'react';
-import { View, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, StyleSheet, ScrollView } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 import HeaderComponent from '../../components/generals/HeaderComponent';
 import AssignLenguaje from '../../lenguage/AssignLenguage';
+import SkeletonComponent from '../../components/generals/SkeletonComponent';
+import NoDataComponent from '../../components/generals/NoDataComponent';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
 const FavoritesScreen = () => {
   const dispatch = useDispatch();
   const textsLeng = useSelector((state) => state.language.texts);
+  const favoritesData = useSelector((state) => state.favorites?.items || []);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     AssignLenguaje(dispatch);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
   }, [dispatch]);
+
+  const skeletonNumber = [1, 2, 3];
 
   return (
     <View style={styles.container}>
       <HeaderComponent title={textsLeng.FavoritesScreen.title} rightIcon='menu-outline' />
+      <ScrollView contentContainerStyle={styles.scrollView} showsVerticalScrollIndicator={false}>
+        {loading ? (
+          skeletonNumber.map((skeleton) => (
+            <View key={skeleton} style={{ marginBottom: hp('2.5%') }}>
+              <SkeletonComponent width={wp('90%')} height={hp('35.8%')} />
+            </View>
+          ))
+        ) : favoritesData.length === 0 ? (
+          <View style={styles.noDataContainer}>
+            <NoDataComponent name="favoritos" icon="heart-outline" />
+          </View>
+        ) : (
+          // Renderizar datos de favoritos aquí
+          <View>
+            {/* Renderizar datos de favoritos */}
+          </View>
+        )}
+      </ScrollView>
     </View>
   );
 };
@@ -23,7 +51,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'white',
-  }
+  },
+  scrollView: {
+    paddingHorizontal: wp('3.75%'),
+  },
+  noDataContainer: {
+    marginTop: hp('20%'),
+  },
 });
 
 export default FavoritesScreen;
